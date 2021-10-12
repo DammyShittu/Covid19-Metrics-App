661a8f74fb082efb457cb94ed86c4023c6329f5b
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-10-12 08:58:06 +0100

    Get Data from Store and Render Countries in Browser

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import { React, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCountriesFromApi } from '../redux/Home/Home';
 
-const Home = () => (
-  <div>
-    <h1>This is the Home Page</h1>
-  </div>
-);
+const Home = () => {
+  const state = useSelector((state) => state.countriesReducer);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCountriesFromApi());
+  }, []);
+
+  return (
+    <ul>
+      {state && state.map((country) => {
+        const { name, confirmedCases, id } = country;
+        return (
+          <li key={id}>
+            <h2>{name}</h2>
+            <h3>{confirmedCases}</h3>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default Home;
