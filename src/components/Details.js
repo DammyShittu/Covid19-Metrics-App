c615f29d1fd62e0c020fb8eb1c7ac6c54d55ec61
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-10-14 13:58:01 +0100

    Create Sub-Components For The Details Page And Style Changes

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import { FaArrowCircleRight } from 'react-icons/fa';
 import Navbar from './Navbar';
-import covid from '../images/covid.png';
+import DetailsTop from './DetailsTop';
+import CountryRegions from './CountryRegions';
 import '../styling/details.css';
 
 const Details = () => {
@@ -12,68 +12,21 @@ const Details = () => {
     casesConfirmed,
     deaths,
     regions,
+    allRecovered,
+    casesOpen,
   } = location.state;
 
   return (
     <>
       <Navbar text={`${countryName} Covid Stats`} />
-      <div className="country-details">
-        <img src={covid} alt="Covid19 Logo" className="covid-img" />
-        <aside>
-          <h2>{countryName}</h2>
-          <div className="details">
-            <div>
-              <span>Total Confirmed </span>
-              <h3>{`${casesConfirmed} cases`}</h3>
-            </div>
-            <div>
-              <span>Total Deaths</span>
-              <h3>{`${deaths} cases`}</h3>
-            </div>
-          </div>
-        </aside>
-      </div>
-      <div className="stats">
-        <h3 className="stats-name">Stats By Region</h3>
-      </div>
-      <ul className="regions">
-        {regions && regions.map((region) => {
-          const {
-            name,
-            today_confirmed: todayConfirmed,
-            today_deaths: todayDeaths,
-          } = region;
-          return (
-            <li key={name} className="region-list">
-              <div>
-                <div className="right">
-                  <FaArrowCircleRight className="arrow" />
-                </div>
-                <h3 className="region-name">{name}</h3>
-                <div className="region-detail">
-                  <div>
-                    <span>Total Confirmed </span>
-                    <h3>{`${todayConfirmed} cases`}</h3>
-                  </div>
-                  <div>
-                    <span>Total Deaths</span>
-                    <h3>{`${todayDeaths} cases`}</h3>
-                  </div>
-                </div>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-
-      {(!regions || !regions[0]) && (
-        <h3 className="apology">
-          We apologize!!
-          <br />
-          There is currently no regional data available for&nbsp;
-          {countryName}
-        </h3>
-      )}
+      <DetailsTop
+        name={countryName}
+        confirmed={casesConfirmed}
+        deaths={deaths}
+        recovered={allRecovered}
+        open={casesOpen}
+      />
+      <CountryRegions regions={regions} countryName={countryName} />
     </>
   );
 };
